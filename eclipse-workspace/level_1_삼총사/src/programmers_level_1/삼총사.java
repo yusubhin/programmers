089f65f4cf86729c55efa05e3cc17807fc75ad57
yusubhin <sdbtnqlss@gmail.com>
2023-01-02 19:10:24 +0900

    first commit

---
+++ eclipse-workspace/level_1_삼총사/src/programmers_level_1/삼총사.java
@@ -0,0 +1,28 @@
+package programmers_level_1;
+
+public class ªÔ√—ªÁ {
+	public static void main(String[] args) {
+        int num;
+        int result;
+        
+        num = 12;
+        result = solution(num);
+        
+        System.out.println(result);
+    }
+    
+    public static int solution(int n) {
+        int answer = 0;
+        int i = 1;
+        
+        while(true) {
+            if ((n % i) == 1)
+                break;
+            else
+                i++;
+        }
+        answer = i;
+        
+        return answer;
+    }
+}
