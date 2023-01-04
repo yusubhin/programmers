927d02c36fa6d32c7a726bb4621e3600f829281d
yusubhin <sdbtnqlss@gmail.com>
2023-01-04 18:19:21 +0900

    third commit

---
+++ eclipse-workspace/PackageTest/src/com/codechobo/book/PackageTest.java
@@ -0,0 +1,10 @@
+package com.codechobo.book;
+
+public class PackageTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello world!");
+	}
+
+}
--- eclipse-workspace/level_1_가장_가까운__같은_글자/src/level_1_가장_가까운__같은_글자/Solution.java
+++ eclipse-workspace/level_1_가장_가까운__같은_글자/src/level_1_가장_가까운__같은_글자/Solution.java
@@ -32,7 +32,7 @@ public class Solution {
 	}
 	
 	public static void main(String[] args) {
-		String str = "newww";
+		String str = "banana";
 		int[] result;
 		
 		result = solution(str);
